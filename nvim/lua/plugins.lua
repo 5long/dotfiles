0673206139b3e0d1e2be59db3c2efa263a253ef7
Whyme Lyu <callme5long@gmail.com>
2023-06-13 19:42:29 +0800

    nmap gr is conflict with existing plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -33,7 +33,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>rf', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
