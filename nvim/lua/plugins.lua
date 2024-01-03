6364a031025769337e5cea09609e693c39a75646
Whyme Lyu <callme5long@gmail.com>
2024-01-04 05:04:12 +0800

    Remove null-ls which is unmaintained

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -164,35 +164,4 @@ return require('packer').startup(function(use)
       }
     }
   end}
-
-  use({ "jose-elias-alvarez/null-ls.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-  })
-
-  local null_ls = require("null-ls")
-
-  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-  null_ls.setup({
-    sources = {
-      null_ls.builtins.completion.luasnip,
-      null_ls.builtins.diagnostics.shellcheck,
-      null_ls.builtins.diagnostics.yamllint,
-      null_ls.builtins.formatting.black,
-    },
-    on_attach = function(client, bufnr)
-      if not client.supports_method("textDocument/formatting") then
-        return
-      end
-
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end,
-  })
 end)
