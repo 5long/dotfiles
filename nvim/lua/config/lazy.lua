928708016b1ce69d82be4a3c32caeb784aabacc8
Whyme Lyu <callme5long@gmail.com>
2025-11-22 16:06:21 +0800

    nvim: more formatting

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -24,7 +24,7 @@ require("lazy").setup({
   install = { colorscheme = { "default" } },
   performance = {
     rtp = {
-      paths = {'/usr/share/vim/vimfiles', '/usr/share/vim/vimfiles/after', },
+      paths = { '/usr/share/vim/vimfiles', '/usr/share/vim/vimfiles/after', },
     }
   },
 })
