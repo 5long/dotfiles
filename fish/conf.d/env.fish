5bd901b80fe525a51f1d8895de9dc9876e4e6a5b
Whyme Lyu <callme5long@gmail.com>
2025-12-24 06:26:43 +0800

    Remove --color=light for sk

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -7,7 +7,6 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 
 set -gx SKIM_DEFAULT_COMMAND 'rg --files --no-require-git'
-set -gx SKIM_DEFAULT_OPTIONS '--color=light'
 set -gx SKIM_CTRL_T_OPTS '--preview="bat -p {}"'
 set -gx MTR_OPTIONS '-n -t'
 
