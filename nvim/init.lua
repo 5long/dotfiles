50fc8c94507744cd88f229bcd8bc558d63ab8078
Whyme Lyu <callme5long@gmail.com>
2026-01-01 16:12:38 +0800

    Revert "nvim: autocomplete command line w/ wildtrigger()"
    This reverts commit 6be62bff107b544728609737cda994bd7b1c7c55.

--- nvim/init.lua
+++ nvim/init.lua
@@ -83,10 +83,6 @@ opt.wildignorecase = true
 opt.completeopt = 'menu,preview,longest'
 opt.shortmess:append('aI')
 opt.showmode = false
-autocmd('CmdlineChanged', {
-  pattern = {":", "/", "?"},
-  callback = function() vim.fn.wildtrigger() end,
-})
 
 opt.ignorecase = true
 opt.smartcase = true
